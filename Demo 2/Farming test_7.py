a7f9d32b5756cd259882a7786542142226930362
camillambrady <47228398+camillambrady@users.noreply.github.com>
2019-05-02 17:19:25 -0500

    Update Farming test_7.py
    with clock and game over screen with score and new sprite

--- Demo 2/Farming test_7.py
+++ Demo 2/Farming test_7.py
@@ -11,8 +11,8 @@ num = 8
 width, height = ((rect_width + margin) * num) + margin, ((rect_height + margin) * num) + margin
 screen = pygame.display.set_mode((width, height))
 tile_coords = {}
-main_char = pygame.image.load('pryor.jpg')
-main_char = pygame.transform.scale(main_char, (50, 50))
+main_char = pygame.image.load('character_front.jpg')
+main_char = pygame.transform.scale(main_char, (45, 45)) #smaller than the actual box so you can see the color of the block you're on
 main_char_rect = main_char.get_rect()
 main_char_rect.centerx = margin + (rect_width / 2)
 main_char_rect.centery = margin + (rect_height / 2)
@@ -23,10 +23,11 @@ score = 0
 
 # Clock initialization
 clock = pygame.time.Clock()
-font = pygame.font.Font(None, 25)
+font_in_game = pygame.font.Font(None, 25)
+font_end_game = pygame.font.Font(None, 40)
 frame_count = 0
 frame_rate = 60
-start_time = 90
+start_time = 20 #small for sake of testing
 
 for row in range(num):
     grid.append([])
@@ -34,19 +35,22 @@ for row in range(num):
     for column in range(num):
         grid[row].append(0)
         timestamp[row].append([])
-
+# main loop
 while running:
     screen.fill(backgroundColor)
     key_in = pygame.key.get_pressed()
-
+ 
     if key_in[pygame.K_ESCAPE]:
         print("Quitting!")
+        print("Score: {}".format(score))
         pygame.quit()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("Quitting!")
+            print("Score: {}".format(score))
             pygame.quit()
+
     column = main_char_rect.centerx // (margin + rect_width)
     row = main_char_rect.centery // (margin + rect_height)
     if key_in[pygame.K_RIGHT] and sum(key_in) == 1 and pressed == 0:
@@ -121,7 +125,8 @@ while running:
                                                 (
                                                             margin + rect_height) * row + margin)})  # update dictionary of tile positions
     screen.blit(main_char, main_char_rect)
-    # Clock printed to screen
+    
+# Clock printed to screen
     total_seconds = start_time - (frame_count // frame_rate)
     if total_seconds < 0:
         total_seconds = 0
@@ -136,7 +141,7 @@ while running:
     output_string = "Time left: {}:{} | Score: {}".format(minutes, seconds, score)  # need to add score variable in this string
 
     # Blit to the screen
-    text = font.render(output_string, True, (0, 0, 0))
+    text = font_in_game.render(output_string, True, (0, 0, 0))
 
     screen.blit(text, [150, 380])
 
@@ -145,5 +150,18 @@ while running:
 
     # Limit frames per second
     clock.tick(frame_rate)
-
+       
     pygame.display.update()
+
+    while (minutes == 0 and seconds == 0):
+        running == False
+        screen.fill((0,0,0))
+        output_string = "Game Over! | Score: {}".format(score)  
+        text = font_end_game.render(output_string, True, (250, 250, 250))
+        screen.blit(text, [50, 200])
+        pygame.display.update()
+        if key_in[pygame.K_ESCAPE]:
+            pygame.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()        
